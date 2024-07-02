6a1598a49b5fb2140018218dc7182113345c19ea
1xa5 <contact@resent.dev>
2024-07-02 01:43:54 -0400

    template

---
+++ main.py
@@ -0,0 +1,26 @@
+import os, dotenv, discord
+from discord.ext import commands
+
+dotenv.load_dotenv(verbose=True)
+
+token=os.environ['token']
+
+class harm(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(command_prefix=";", owner_ids="214753146512080907", 
+                       intents=discord.Intents.all())
+
+os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
+os.environ["JISHAKU_HIDE"] = "True"
+os.environ["JISHAKU_FORCE_PAGINATOR"] = "True"
+os.environ["JISHAKU_RETAIN"] = "True"
+
+bot = harm()
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    await bot.load_extension('jishaku')
+
+bot.run(token)
